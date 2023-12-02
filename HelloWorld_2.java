30e6ff0e5b45752b33bf2f80607a5193f00a48aa
Francisco Cruz <francisco.cruzg.gmail.com>
2023-12-02 12:00:27 -0600

    hot fix

--- HelloWorld_2.java
+++ HelloWorld_2.java
@@ -2,6 +2,7 @@ public class HelloWorld_2{
 
     public static void main (String args[]){
         System.err.println("Hola Mundo 2 cambio");
+        System.err.println("Hola Mundo 2 cambio urgente");
     }
 
 }

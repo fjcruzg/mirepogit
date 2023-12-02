e5fa2563ce4463910458e15760209ab7f5c412a6
Francisco Cruz <francisco.cruzg.gmail.com>
2023-12-02 12:21:28 -0600

    Correcciones en master

--- HelloWorld_2.java
+++ HelloWorld_2.java
@@ -2,15 +2,10 @@ public class HelloWorld_2{
 
     public static void main (String args[]){
         System.err.println("Hola Mundo 2 cambio");
-<<<<<<< HEAD
         System.err.println("Hola Mundo 2 cambio urgente");
-=======
         System.err.println("Hot fix en dev");
          System.err.println("Cambio importante para mi otra rama");
-<<<<<<< HEAD
->>>>>>> 3ae6dcc (Subiendo cambio importante desde feature)
-=======
->>>>>>> 3ae6dcc (Subiendo cambio importante desde feature)
+
     }
 
 }

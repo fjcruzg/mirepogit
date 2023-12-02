222076c32ebee8004108ac7729dfc950374db7bc
Francisco Cruz <francisco.cruzg.gmail.com>
2023-12-02 12:21:28 -0600

    Cambios desde DEV

--- HelloWorld_2.java
+++ HelloWorld_2.java
@@ -2,7 +2,12 @@ public class HelloWorld_2{
 
     public static void main (String args[]){
         System.err.println("Hola Mundo 2 cambio");
+<<<<<<< HEAD
         System.err.println("Hola Mundo 2 cambio urgente");
+=======
+        System.err.println("Hot fix en dev");
+         System.err.println("Cambio importante para mi otra rama");
+>>>>>>> 3ae6dcc (Subiendo cambio importante desde feature)
     }
 
 }
